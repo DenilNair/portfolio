26b86b22519b53fdafe69d733e9fe1a81756f195
DenilNair <denil007nair@gmail.com>
2023-06-02 17:11:51 +0530

    add:firebase

--- src/app/navbar/blogs/blogs.component.ts
+++ src/app/navbar/blogs/blogs.component.ts
@@ -21,7 +21,7 @@ export class BlogsComponent {
   }
 
   blogs: Blog[] = [
-    {id: 1, title: 'Deploy Image to Kubernetes and know about Kubectl commands',image:'./assets/kubernetes-docker.jpg' ,description: '', link: 'white'},
+    {id: 1, title: 'Deploy Image to Kubernetes and know about Kubectl commands',image:'./assets/kubernetes-docker.png' ,description: '', link: 'white'},
     {id: 2, title: 'Docker Compose for Spring boot Microservices',image:'./assets/docker_logo.png' , description: '', link: 'white'},
     {id: 3, title: 'Few Steps towards Docker ..',image:'./assets/docker_logo.png' , description: '', link: 'white'},
     {id: 4, title: 'First REST API using Spring boot',image:'./assets/docker_logo.png' , description: '', link: 'white'},
