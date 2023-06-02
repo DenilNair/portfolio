544ee87c8b6383c74470d52246b4a7d414c70de3
DenilNair <denil007nair@gmail.com>
2023-06-02 09:42:45 +0530

    add: blog component features

--- src/app/navbar/blogs/blogs.component.ts
+++ src/app/navbar/blogs/blogs.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
+export interface Blog {
+  id: number;
+  title: string;
+  description: string;
+  link: string;
+  image:string;
 
+}
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
@@ -12,4 +19,13 @@ export class BlogsComponent {
   toggleGridColumns() {
     this.gridColumns = this.gridColumns === 3 ? 4 : 3;
   }
+
+  blogs: Blog[] = [
+    {id: 1, title: 'Deploy Image to Kubernetes and know about Kubectl commands',image:'./assets/kubernetes-docker.jpg' ,description: '', link: 'white'},
+    {id: 2, title: 'Docker Compose for Spring boot Microservices',image:'./assets/docker_logo.png' , description: '', link: 'white'},
+    {id: 3, title: 'Few Steps towards Docker ..',image:'./assets/docker_logo.png' , description: '', link: 'white'},
+    {id: 4, title: 'First REST API using Spring boot',image:'./assets/docker_logo.png' , description: '', link: 'white'},
+    {id: 5, title: '',image:'./assets/spring_rest.png' , description: '', link: 'white'},
+
+  ];
 }
